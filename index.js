3af52bfa01590dc983c389eec51e0e50038fb405
yohelperez <perezyohel@gmail.com>
2021-07-11 22:22:03 -0500

    Adding listeners to the keyboard

--- index.js
+++ index.js
@@ -4,3 +4,27 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter: This is fun :)</h1>`;
+
+const keyboard = document.getElementById("keyboard");
+
+/*
+keyboard.addEventListener('click', function(event){
+  alert(`You clicked on ${event.target.innerText}`);
+});
+*/
+
+keyboard.addEventListener('click', function(event){
+  keyPressed(event);
+});
+
+function keyPressed(){
+  alert(`You clicked on ${event.target.innerText}`);
+
+}
+
+function writeNumbers(){
+
+
+}
+
+
